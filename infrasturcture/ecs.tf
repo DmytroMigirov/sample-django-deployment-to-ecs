5ee2c654745b49cdfae16247175e1bef946121aa
DmytroMigirov <dmytromigirov@gmail.com>
2024-04-15 17:06:34 +0200

    Fix migrate in dockerfile

--- infrasturcture/ecs.tf
+++ infrasturcture/ecs.tf
@@ -19,13 +19,48 @@ resource "aws_ecs_task_definition" "app" {
   family                   = "django-app"
   network_mode             = "awsvpc" # Required for Fargate
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "${var.fargate_cpu}"
-  memory                   = "${var.fargate_memory}"
+  cpu                      = var.fargate_cpu
+  memory                   = var.fargate_memory
   execution_role_arn       = aws_iam_role.ecs-task-execution-role.arn
   task_role_arn            = aws_iam_role.ecs-task-execution-role.arn
-  container_definitions    = data.template_file.app.rendered
-  depends_on               = [aws_db_instance.production]
+  container_definitions    = jsonencode([
+    {
+      name        = "django-migration-container"
+      image       = var.docker_image_url_django
+      environment = [
+        {
+          name  = "RDS_DB_NAME"
+          value = var.rds_db_name
+        },
+        {
+          name  = "RDS_USERNAME"
+          value = var.rds_username
+        },
+        {
+          name  = "RDS_PASSWORD"
+          value = var.rds_password
+        },
+        {
+          name  = "RDS_HOSTNAME"
+          value = aws_db_instance.production.address
+        },
+        {
+          name  = "DATABASE_URL"
+          value = "postgresql://${var.rds_username}:${var.rds_password}@${aws_db_instance.production.address}:5432/${var.rds_db_name}"
+        },
+        {
+          name  = "RDS_PORT"
+          value = "5432"
+        }
+      ]
+      # Run migrate command
+      command = ["python", "manage.py", "migrate"]
+    }
+  ])
+
+  depends_on = [aws_db_instance.production]
 }
+
 
 resource "aws_ecs_service" "production" {
   name            = "${var.ecs_cluster_name}-service"
